88cadf1cfa2757c3dc666426f66bd9e2306c70b5
Bruno Duarte <brsants@gmail.com>
2025-06-06 18:32:06 -0300

    chore: remove data directory

--- database/init-scripts/init.sql
+++ database/init-scripts/init.sql
@@ -1,4 +1,4 @@
-create type voting_status as enum('created', 'open', 'closed', 'canceled');
+create type voting_status as enum('open', 'closed', 'canceled');
 create type voting_result as enum('accepted', 'rejected', 'undefined');
 
 create table if not exists associated (
@@ -7,27 +7,34 @@ create table if not exists associated (
 	email VARCHAR(100) unique not NULL,
 	phone VARCHAR(20)
 );
+create INDEX associated_name_idx on associated(name);
 
 create table if not exists assembly (
 	id UUID primary key default gen_random_uuid(),
 	day DATE not NULL
 );
+create INDEX assembly_day_idx on assembly(day);
 
 create table if not exists subject (
 	id UUID primary key default gen_random_uuid(),
 	headline VARCHAR(100) not null,
 	description TEXT not NULL
 );
+create INDEX subject_headline_idx on subject(headline);
 
 create table if not exists voting (
 	id UUID primary key default gen_random_uuid(),
 	subject UUID references subject(id),
-	created_in TIMESTAMP not null,
-	opened_in TIMESTAMP,
-	closed_in TIMESTAMP,
-	status voting_status not null,
-	result voting_result
+	voting_interval INTERVAL default '1 minute',
+	opened_in TIMESTAMP default now(),
+	closes_in TIMESTAMP,
+	status voting_status default 'open',
+	result voting_result,
+	votes_in_favor INTEGER default 0,
+	votes_against INTEGER default 0
 );
+create INDEX voting_status_idx on voting(status);
+create INDEX voting_result_idx on voting(result);
 
 create table if not exists vote (
 	id UUID primary key default gen_random_uuid(),
